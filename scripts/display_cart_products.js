e15eab72a53195d4260fc2d822006d4b3f59ed87
Thomas <thomas.bergamin@placemeet.fr>
2021-09-02 22:12:54 +0200

    connect display shopping cart to session storage

--- scripts/display_cart_products.js
+++ scripts/display_cart_products.js
@@ -1,15 +1,55 @@
-const getProductsInCart = () => {
-  let products = sessionStorage.getItem("cart");
-  return products;
+const getCamerasFromCart = () => {
+  const camerasInCart = JSON.parse(sessionStorage.getItem("cart"));
+  return camerasInCart;
 };
 
-const renderProducts = () => {
-  let cameras = getProductsInCart();
+const displayShoppingCart = () => {
+  const cameras = getCamerasFromCart();
+
+  let contentToDisplay = `<div class="alert alert-primary" role="alert">
+    You have no product in your cart :-(</div>`;
+
   if (cameras) {
-    console.log(cameras);
-  } else {
-    // display nothing in cart -> go back to index to shop
+    let tableRowsHTML = "";
+    let rowIndex = 1;
+
+    cameras.forEach((camera) => {
+      let row = `<tr>
+                <th scope="row">${rowIndex}</th>
+                <td>${camera.lens}</td>
+                <td>${camera.lens}</td>
+                <td>${camera.cameraId}</td>
+              </tr>`;
+      tableRowsHTML = tableRowsHTML.concat(row);
+    });
+
+    let tableHTML = `<table class="table table-striped table-hover table-bordered">
+            <thead class="table-primary">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Camera Name</th>
+                <th scope="col">Lens</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${tableRowsHTML}
+            </tbody>
+            <tfoot class="table-primary">
+              <tr>
+                <th scope="row">Total</th>
+                <td>--</td>
+                <td>--</td>
+                <td class="fw-bold">570€</td>
+              </tr>
+            </tfoot>
+          </table>`;
+    contentToDisplay = tableHTML;
   }
+
+  let container = document.getElementById("emptyDiv");
+  container.innerHTML = contentToDisplay;
+  return;
 };
 
-renderProducts();
+displayShoppingCart();
