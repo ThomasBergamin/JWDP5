0d1d8580769d0d0448126b5690ae3b07b6405163
Thomas <thomas.bergamin@placemeet.fr>
2021-09-03 10:29:02 +0200

    create module for render cart label

--- scripts/render_cart_label.js
+++ scripts/render_cart_label.js
@@ -1,13 +1,3 @@
-const renderCartLabel = () => {
-  const camerasInCart = JSON.parse(sessionStorage.getItem("cart"));
-  let container = document.getElementById("emptyNotification");
-  if (camerasInCart) {
-    let quantity = camerasInCart.length;
-    const htmlNotification = `<span class="badge bg-danger rounded-pill">${quantity}</span>`;
-    container.innerHTML = htmlNotification;
-  } else {
-    container.innerHTML = "";
-  }
-};
+import { renderCartLabel } from "../modules/renderCartLabel.js";
 
-renderCartLabel();
+window.onload = renderCartLabel();
